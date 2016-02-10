bb44687da136535a876ac3ca0049ede73a41f24f
Haoha <haojie.huang@yahoo.fr>
2016-02-10 14:26:35 +0100

    begin camagru

---
+++ rendu/Common.php
@@ -0,0 +1,9 @@
+<?php
+
+session_start();
+include_once('bd.php');
+function __autoload($class_name) {
+    include_once 'Models/'.$class_name . '.php';
+}
+
+?>
---
+++ rendu/index.php
@@ -0,0 +1,22 @@
+<?php
+	include_once ('Common.php');
+?>
+
+<!DOCTYPE html>
+<html>
+	<?php
+	include ('Ressources/views/partials/head.php');
+	?>
+	<body>
+		<div class="container">
+			<header>
+
+			</header> 
+
+			<footer>
+
+			</footer>
+		</div>
+		<script src="js/javascript.js"> </script>
+	</body>
+</html>
