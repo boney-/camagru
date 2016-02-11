87eccdbee691ede3cc4fbbcafb79a06dde37a63a
Haojie HUANG <hhuang@e2r4p19.42.fr>
2016-02-11 17:14:29 +0100

    new rendu

---
+++ new_rendu/inc/header.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="img/favicon.ico">
+
+    <title>Camagru</title>
+
+    <link href="css/reset.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <nav class="navbar">
+      <div class="container">
+        <div class="brand_div">
+          <a class="brand" href="#"><h1>Camagru</h1></a>
+        </div>
+        <div id="navbar">
+          <ul class="nav">
+            <li><a href="register.php">S'inscrire</a></li>
+            <li><a href="login.php">Se connecter</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
