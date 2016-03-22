316ffab0e7178f98d967d85ac4796d2cbfe25788
Haojie HUANG <hhuang@e1r8p17.42.fr>
2016-03-22 17:35:03 +0100

    change footer

--- new_rendu/inc/footer.php
+++ new_rendu/inc/footer.php
@@ -1,9 +1,6 @@
 </div>
-<<<<<<< Updated upstream
+
 <!--	<script src="js/show_upload.js"></script>-->
-=======
-	<script src="js/show_upload.js"></script>
 	<script src="js/select_filter.js"></script>
->>>>>>> Stashed changes
 </body>
 </html>
